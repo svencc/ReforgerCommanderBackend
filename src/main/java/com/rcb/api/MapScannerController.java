57a0c9127b4d81c281c6a6460faf235cc2e91c43
Sven Carrillo Castillo <carrillo.castillo.sven@googlemail.com>
2023-05-27 07:47:06 +0200

    some renaming

--- src/main/java/com/rcb/api/MapScannerController.java
+++ src/main/java/com/rcb/api/MapScannerController.java
@@ -1,7 +1,7 @@
 package com.rcb.api;
 
 import com.rcb.api.commons.HttpCommons;
-import com.rcb.dto.mapScanner.MapScannerEntitiesDto;
+import com.rcb.dto.mapScanner.MapScannerEntityPackageDto;
 import com.rcb.dto.mapScanner.MapScannerEntityDto;
 import com.rcb.dto.mapScanner.TransactionIdentifierDto;
 import com.rcb.util.ReforgerPayload;
@@ -20,7 +20,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 
@@ -112,7 +111,7 @@ public class MapScannerController {
         HttpStatus.OK.value();
         log.debug("Requested POST /api/v1/test/map-entity");
 
-        final Optional<MapScannerEntitiesDto> mapScannerEntitiesOpt = ReforgerPayload.parse(payload, MapScannerEntitiesDto.class);
+        final Optional<MapScannerEntityPackageDto> mapScannerEntitiesOpt = ReforgerPayload.parse(payload, MapScannerEntityPackageDto.class);
         log.debug(" -> Received: {}", mapScannerEntitiesOpt);
 
         return ResponseEntity.status(HttpStatus.OK)
--- src/main/java/com/rcb/dto/mapScanner/MapScannerEntityPackageDto.java
+++ src/main/java/com/rcb/dto/mapScanner/MapScannerEntityPackageDto.java
@@ -8,8 +8,6 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.math.BigDecimal;
-import java.util.ArrayList;
 import java.util.List;
 
 @Data
@@ -18,7 +16,7 @@ import java.util.List;
 @NoArgsConstructor
 @AllArgsConstructor
 @JsonInclude(JsonInclude.Include.NON_NULL)
-public class MapScannerEntitiesDto {
+public class MapScannerEntityPackageDto {
 
     @Schema
     @JsonProperty()
