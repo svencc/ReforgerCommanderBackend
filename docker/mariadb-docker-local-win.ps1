ecb6458ce8f363c220557298f2c7b8f53367372d
Sven Carrillo Castillo <carrillo.castillo.sven@googlemail.com>
2023-05-28 09:34:08 +0200

    improve mariadb-docker-local-win.ps1

--- docker/mariadb-docker-local-win.ps1
+++ docker/mariadb-docker-local-win.ps1
@@ -17,7 +17,7 @@ $DOCKER_DBCONTAINER_FILTER_STRING = (docker container ls --all)
 echo "${DOCKER_DBCONTAINER_FILTER_STRING}"
 
 if ($DOCKER_DBCONTAINER_FILTER_STRING.endsWith($MARIADB_CONTAINER_NAME)) {
-  echo "Container ${MARIADB_CONTAINER_NAME} was found!"
+  echo "Container $MARIADB_CONTAINER_NAME was found!"
   # start if exists
   docker start ${MARIADB_CONTAINER_NAME}
   # wait until db is startet and initialized
@@ -28,14 +28,14 @@ if ($DOCKER_DBCONTAINER_FILTER_STRING.endsWith($MARIADB_CONTAINER_NAME)) {
   echo "Container $MARIADB_CONTAINER_NAME is missing!"
   # create container:
   mkdir -Force "${PROJECT_FOLDER}\..\${MARIADB_DATA_DIRECTORY}"
-  docker run -p ${MARIADB_PORT}:3306 -v ${PROJECT_FOLDER}/../${MARIADB_DATA_DIRECTORY}:/var/lib/mysql --detach --name ${MARIADB_CONTAINER_NAME} --env MARIADB_USER=${MARIADB_USER} --env MARIADB_PASSWORD=${MARIADB_PASSWORD} --env MARIADB_ROOT_PASSWORD=${MARIADB_ROOT_PASSWORD} mariadb:latest
-  docker start ${MARIADB_CONTAINER_NAME}
+  iex "docker run -p ${MARIADB_PORT}:3306 -v ${PROJECT_FOLDER}/../${MARIADB_DATA_DIRECTORY}:/var/lib/mysql --detach --name ${MARIADB_CONTAINER_NAME} --env MARIADB_USER=${MARIADB_USER} --env MARIADB_PASSWORD=${MARIADB_PASSWORD} --env MARIADB_ROOT_PASSWORD=${MARIADB_ROOT_PASSWORD} mariadb:latest"
+  iex "docker start ${MARIADB_CONTAINER_NAME}"
   # wait until db is startet and initialized
   sleep 10
-  echo "Create ${MARIADB_CONTAINER_NAME DB!}"
+  echo "Create $MARIADB_CONTAINER_NAME DB!"
   # run mariadb-docker-local cli, execute CREATE DATABASE query
-  docker run --name ${MARIADB_CONTAINER_NAME}-cli -it --rm mariadb mysql -hhost.docker.internal --port=${MARIADB_PORT} -uroot -p${MARIADB_ROOT_PASSWORD} -e CREATE DATABASE ${PROJECT_DB_NAME};
+  iex "docker run --name ${MARIADB_CONTAINER_NAME}-cli -it --rm mariadb mysql -hhost.docker.internal --port=${MARIADB_PORT} -uroot -p${MARIADB_ROOT_PASSWORD} -e CREATE DATABASE ${PROJECT_DB_NAME};"
   # run mariadb-docker-local cli, connected to running
-  docker run --name ${MARIADB_CONTAINER_NAME}-cli -it --rm mariadb mysql -hhost.docker.internal --port=${MARIADB_PORT} -uroot -p${MARIADB_ROOT_PASSWORD}
+  iex "docker run --name ${MARIADB_CONTAINER_NAME}-cli -it --rm mariadb mysql -hhost.docker.internal --port=${MARIADB_PORT} -uroot -p${MARIADB_ROOT_PASSWORD}"
   sleep 999
 }
