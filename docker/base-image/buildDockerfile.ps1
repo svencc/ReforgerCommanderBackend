726a55d15169e3c2b95d457a0e2240badce06eba
Sven Carrillo Castillo <carrillo.castillo.sven@googlemail.com>
2023-05-29 16:52:54 +0200

    add some docker and build related stuff and app config -> caching ready

---
+++ docker/base-image/buildDockerfile.ps1
@@ -0,0 +1,20 @@
+#!/c/Windows/System32/WindowsPowerShell/v1.0/powershell.exe -File
+
+# DEFINE INPUT PARAMETER --------------------------------------------------------------------------------------------- #
+$DOCKER_VERSION_TAG = $args[0]
+
+## validate input parameters
+if ($args.count -ne 1) {
+  echo "Invalid nr of arguments supplied; Expected <DOCKER_VERSION_TAG>"
+  exit 255
+}
+
+# END DEFINE INPUT PARAMETER ------------------------------------------------------------------------------------- END #
+
+# ==> MAIN LOGIC <== #
+docker build -t "svencc/ref-com-base:$DOCKER_VERSION_TAG" .
+docker tag "svencc/ref-com-base:$DOCKER_VERSION_TAG" "svencc/ref-com-base:$DOCKER_VERSION_TAG"
+docker push "svencc/ref-com-base:$DOCKER_VERSION_TAG"
+
+echo "svencc/ref-com-base:$DOCKER_VERSION_TAG"
+exit 0
