90066706249f9ede602529685e71b162dd5892f8
Sven Carrillo Castillo <carrillo.castillo.sven@googlemail.com>
2023-07-22 23:22:53 +0200

    add another 2 transaction tests

--- src/test/java/com/recom/event/MapEntityScannerTransactionEventListenerTest.java
+++ src/test/java/com/recom/event/MapEntityScannerTransactionEventListenerTest.java
@@ -3,6 +3,7 @@ package com.recom.event;
 import com.recom.dto.map.scanner.TransactionIdentifierDto;
 import com.recom.dto.map.scanner.TransactionalEntityPackageDto;
 import com.recom.event.event.async.map.AddMapPackageAsyncEvent;
+import com.recom.event.event.async.map.CommitMapTransactionAsyncEvent;
 import com.recom.event.event.async.map.OpenMapTransactionAsyncEvent;
 import com.recom.event.event.sync.cache.CacheResetSyncEvent;
 import com.recom.model.map.MapTransaction;
@@ -65,14 +66,15 @@ public class MapEntityScannerTransactionEventListenerTest {
     public void testHandleAddMapPackage_whenTransactionExists_shouldIgnoreEventAndDoNothing() {
         // Arrange
         final TransactionalEntityPackageDto packageDto = new TransactionalEntityPackageDto();
-        packageDto.setSessionIdentifier("session1");
+        final String session1 = "session1";
+        packageDto.setSessionIdentifier(session1);
         final AddMapPackageAsyncEvent event = new AddMapPackageAsyncEvent(packageDto);
 
         // Act
         eventListener.handleAddMapPackage(event);
 
         // Assert
-        assertFalse(eventListener.getTransactions().containsKey("session1"));
+        assertFalse(eventListener.getTransactions().containsKey(session1));
         verify(mapEntityPersistenceLayer, never()).deleteMapEntities(anyString());
         verify(mapEntityPersistenceLayer, never()).saveAll(anyList());
         verify(applicationEventPublisher, never()).publishEvent(any(CacheResetSyncEvent.class));
@@ -82,101 +84,72 @@ public class MapEntityScannerTransactionEventListenerTest {
     public void testHandleAddMapPackage_whenTransactionIsOpenedJustBefore_shouldAddPackageAndDoNotPersist() {
         // Arrange
         final TransactionalEntityPackageDto packageDto = new TransactionalEntityPackageDto();
-        packageDto.setSessionIdentifier("session1");
+        final String session1 = "session1";
+        packageDto.setSessionIdentifier(session1);
         final AddMapPackageAsyncEvent event = new AddMapPackageAsyncEvent(packageDto);
 
         // Act
-        final OpenMapTransactionAsyncEvent openSessionEvent = new OpenMapTransactionAsyncEvent(TransactionIdentifierDto.builder().sessionIdentifier("session1").build());
+        // open transaction and send event/package
+        final OpenMapTransactionAsyncEvent openSessionEvent = new OpenMapTransactionAsyncEvent(TransactionIdentifierDto.builder().sessionIdentifier(session1).build());
         eventListener.handleOpenTransaction(openSessionEvent);
         eventListener.handleAddMapPackage(event);
 
         // Assert
-        assertTrue(eventListener.getTransactions().containsKey("session1"));
-        assertEquals(1, eventListener.getTransactions().get("session1").getPackages().size());
-        assertTrue(eventListener.getTransactions().get("session1").getPackages().contains(packageDto));
+        assertTrue(eventListener.getTransactions().containsKey(session1));
+        assertEquals(1, eventListener.getTransactions().get(session1).getPackages().size());
+        assertTrue(eventListener.getTransactions().get(session1).getPackages().contains(packageDto));
         verify(mapEntityPersistenceLayer, never()).deleteMapEntities(anyString());
         verify(mapEntityPersistenceLayer, never()).saveAll(anyList());
         verify(applicationEventPublisher, never()).publishEvent(any(CacheResetSyncEvent.class));
     }
 
-//    @Test
-//    public void testProcessTransaction_ValidTransaction() {
-//        // Arrange
-//        final TransactionalEntityPackageDto packageDto = new TransactionalEntityPackageDto();
-//        final MapTransaction transaction = MapTransaction.builder().build();;
-//        packageDto.setEntities(new ArrayList<>());
-//        transaction.getPackages().add(packageDto);
-//        final List<MapEntity> entities = new ArrayList<>();
-//        when(mapEntityPersistenceLayer.saveAll(anyList())).thenReturn(entities);
-//        when(mapTransactionValidator.isValidTransaction(any(MapTransaction.class))).thenReturn(true);
-//
-//        // Act
-//        boolean isProcessed = eventListener.processTransaction("session1");
-//
-//        // Assert
-//        assert isProcessed;
-//        verify(mapEntityPersistenceLayer, times(1)).deleteMapEntities("session1");
-//        verify(mapEntityPersistenceLayer, times(1)).saveAll(anyList());
-//    }
-//
-//    @Test
-//    public void testProcessTransaction_InvalidTransaction() {
-//        // Arrange
-//        final MapTransaction transaction = new MapTransaction();
-//        TransactionalEntityPackageDto packageDto = new TransactionalEntityPackageDto();
-//        packageDto.setEntities(new ArrayList<>());
-//        transaction.getPackages().add(packageDto);
-//        when(mapTransactionValidator.isValidTransaction(any(MapTransaction.class))).thenReturn(false);
-//
-//        // Act
-//        boolean isProcessed = eventListener.processTransaction("session1");
-//
-//        // Assert
-//        assert !isProcessed;
-//        verify(mapEntityPersistenceLayer, never()).deleteMapEntities(anyString());
-//        verify(mapEntityPersistenceLayer, never()).saveAll(anyList());
-//    }
-//
-//    @Test
-//    public void testHandleCommitTransaction_ExistingTransaction() {
-//        // Arrange
-//        CommitMapTransactionAsyncEvent event = new CommitMapTransactionAsyncEvent();
-//        TransactionIdentifierDto transactionIdentifierDto = new TransactionIdentifierDto();
-//        transactionIdentifierDto.setSessionIdentifier("session1");
-//        event.setTransactionIdentifierDto(transactionIdentifierDto);
-//
-//        Map<String, MapTransaction> transactions = new HashMap<>();
-//        MapTransaction transaction = new MapTransaction();
-//        transactions.put("session1", transaction);
-//        eventListener.setTransactions(transactions);
-//
-//        // Act
-//        eventListener.handleCommitTransaction(event);
-//
-//        // Assert
-//        assert transaction.getCommitTransactionIdentifier().equals(transactionIdentifierDto);
-//        verify(mapEntityPersistenceLayer, times(1)).deleteMapEntities("session1");
-//        verify(mapEntityPersistenceLayer, times(1)).saveAll(anyList());
-//    }
-//
-//    @Test
-//    public void testHandleCommitTransaction_NoTransactionFound() {
-//        // Arrange
-//        CommitMapTransactionAsyncEvent event = new CommitMapTransactionAsyncEvent();
-//        TransactionIdentifierDto transactionIdentifierDto = new TransactionIdentifierDto();
-//        transactionIdentifierDto.setSessionIdentifier("session1");
-//        event.setTransactionIdentifierDto(transactionIdentifierDto);
-//
-//        Map<String, MapTransaction> transactions = new HashMap<>();
-//        eventListener.setTransactions(transactions);
-//
-//        // Act
-//        eventListener.handleCommitTransaction(event);
-//
-//        // Assert
-//        assert !transactions.containsKey("session1");
-//        verify(mapEntityPersistenceLayer, never()).deleteMapEntities(anyString());
-//        verify(mapEntityPersistenceLayer, never()).saveAll(anyList());
-//    }
+    @Test
+    public void testHandleCommitTransaction_whenTransactionIsValid_shouldProcessTransaction() {
+        // Arrange
+        final TransactionalEntityPackageDto packageDto = new TransactionalEntityPackageDto();
+        final String session1 = "session1";
+        packageDto.setSessionIdentifier(session1);
+        final AddMapPackageAsyncEvent event = new AddMapPackageAsyncEvent(packageDto);
+        when(mapTransactionValidator.isValidTransaction(any(MapTransaction.class))).thenReturn(true);
+
+        // Act
+        // open transaction and send event/package
+        final OpenMapTransactionAsyncEvent openTransactionEvent = new OpenMapTransactionAsyncEvent(TransactionIdentifierDto.builder().sessionIdentifier(session1).build());
+        eventListener.handleOpenTransaction(openTransactionEvent);
+
+        eventListener.handleAddMapPackage(event);
+
+        final CommitMapTransactionAsyncEvent commitEvent = new CommitMapTransactionAsyncEvent(TransactionIdentifierDto.builder().sessionIdentifier(session1).build());
+        eventListener.handleCommitTransaction(commitEvent);
+
+        // Assert
+        verify(mapTransactionValidator, times(1)).isValidTransaction(eq(eventListener.getTransactions().get(session1)));
+        verify(transactionTemplate, times(1)).execute(any());
+    }
+
+
+    @Test
+    public void testHandleCommitTransaction_whenTransactionIsInvalid_shouldNotProcessTransaction() {
+        // Arrange
+        final TransactionalEntityPackageDto packageDto = new TransactionalEntityPackageDto();
+        final String session1 = "session1";
+        packageDto.setSessionIdentifier(session1);
+        final AddMapPackageAsyncEvent event = new AddMapPackageAsyncEvent(packageDto);
+        when(mapTransactionValidator.isValidTransaction(any(MapTransaction.class))).thenReturn(false);
+
+        // Act
+        // open transaction and send event/package
+        final OpenMapTransactionAsyncEvent openTransactionEvent = new OpenMapTransactionAsyncEvent(TransactionIdentifierDto.builder().sessionIdentifier(session1).build());
+        eventListener.handleOpenTransaction(openTransactionEvent);
+
+        eventListener.handleAddMapPackage(event);
+
+        final CommitMapTransactionAsyncEvent commitEvent = new CommitMapTransactionAsyncEvent(TransactionIdentifierDto.builder().sessionIdentifier(session1).build());
+        eventListener.handleCommitTransaction(commitEvent);
+
+        // Assert
+        verify(mapTransactionValidator, times(1)).isValidTransaction(eq(eventListener.getTransactions().get(session1)));
+        verify(transactionTemplate, never()).execute(any());
+    }
 
 }
