fffae291d02b90a1f82530fae355c99bf7db0b72
Sven Carrillo Castillo <carrillo.castillo.sven@googlemail.com>
2023-09-08 17:56:40 +0200

    fix getter final issue

--- src/main/java/lib/goap/state/GoapState.java
+++ src/main/java/lib/goap/state/GoapState.java
@@ -7,7 +7,7 @@ import lombok.NonNull;
 public class GoapState implements Stateful, Important {
 
     @NonNull
-    private final Integer importance;
+    private Integer importance;
     @NonNull
     private final String effect;
     @NonNull
