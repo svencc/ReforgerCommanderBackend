a1c2332bc2a12bf4ec7997c4cc66bbd760761214
Sven Carrillo Castillo <carrillo.castillo.sven@googlemail.com>
2023-10-19 22:16:03 +0200

    tidy up

--- src/test/java/com/recom/service/messagebus/MessageLongPollObserverTest.java
+++ src/test/java/com/recom/service/messagebus/MessageLongPollObserverTest.java
@@ -12,7 +12,6 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.core.task.AsyncTaskExecutor;
 
 import java.util.List;
 
