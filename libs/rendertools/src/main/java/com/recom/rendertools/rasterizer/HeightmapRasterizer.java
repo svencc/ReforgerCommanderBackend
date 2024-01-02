dbd08c51d07a8eb10b5fedc10020d702652c0d63
Sven Carrillo Castillo <carrillo.castillo.sven@googlemail.com>
2024-01-02 17:28:18 +0100

    optimize heightmap rasterization

--- libs/rendertools/src/main/java/com/recom/rendertools/rasterizer/HeightmapRasterizer.java
+++ libs/rendertools/src/main/java/com/recom/rendertools/rasterizer/HeightmapRasterizer.java
@@ -5,64 +5,29 @@ import lombok.NonNull;
 import javax.imageio.ImageIO;
 import java.awt.*;
 import java.awt.image.BufferedImage;
+import java.awt.image.DataBufferInt;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 
 public class HeightmapRasterizer {
 
     @NonNull
-    @Deprecated
-    private ByteArrayOutputStream rasterizeHeightMapPNGold(@NonNull final HeightMapDescriptor command) throws IOException {
-        final int width = command.getHeightMap().length;
-        final int height = command.getHeightMap()[0].length;
-        final BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
-
-        final float heightRange = command.getMaxHeight() - command.getSeaLevel();
-        final float depthRange = command.getMaxWaterDepth() - command.getSeaLevel();
-
-        for (int x = 0; x < width; x++) {
-            for (int z = 0; z < height; z++) {
-                final float heightValue = command.getHeightMap()[x][z];
-                Color color;
-
-                if (heightValue >= command.getSeaLevel()) {
-                    // map height to color
-                    final float dynamicHeightValue = (heightValue - command.getSeaLevel()) / heightRange;
-                    int grayValue = (int) (255 * dynamicHeightValue); // normalize to 0..255
-                    grayValue = Math.min(Math.max(grayValue, 0), 255); // ensure that the value is in the valid range
-                    color = new Color(grayValue, grayValue, grayValue);
-                } else {
-                    // map depth to water color
-                    final float dynamicDepthValue = (heightValue - command.getSeaLevel()) / depthRange;
-                    int blueValue = (int) (255 * (dynamicDepthValue)); //  // normalize to 0..255
-                    blueValue = Math.min(Math.max(blueValue, 0), 255);
-                    color = new Color((int) (blueValue * 0.77), (int) (192 * 0.94), blueValue);
-                }
-
-                image.setRGB(x, z, color.getRGB());
-            }
-        }
-
-        final ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-        ImageIO.write(image, "png", outputStream);
-
-        return outputStream;
-    }
-
-    @NonNull
     public ByteArrayOutputStream rasterizeHeightMapPNG(@NonNull final HeightMapDescriptor command) throws IOException {
-        @NonNull int[] imageBuffer = rasterizeHeightMap(command);
+        @NonNull int[] pixelBuffer = rasterizeHeightMapRGB(command);
 
         final int width = command.getHeightMap().length;
         final int height = command.getHeightMap()[0].length;
         final BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
 
-        for (int x = 0; x < width; x++) {
-            for (int z = 0; z < height; z++) {
-                final int rgb = imageBuffer[x + z * width]; // get the color from the int buffer array
-                image.setRGB(x, z, rgb);
-            }
-        }
+//        for (int x = 0; x < width; x++) {
+//            for (int z = 0; z < height; z++) {
+//                final int rgb = pixelBuffer[x + z * width]; // get the color from the int buffer array
+//                image.setRGB(x, z, rgb);
+//            }
+//        }
+
+        final int[] imagePixels = ((DataBufferInt) image.getRaster().getDataBuffer()).getData();
+        System.arraycopy(pixelBuffer, 0, imagePixels, 0, pixelBuffer.length);
 
         final ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
         ImageIO.write(image, "png", outputStream);
@@ -71,7 +36,7 @@ public class HeightmapRasterizer {
     }
 
     @NonNull
-    public int[] rasterizeHeightMap(@NonNull final HeightMapDescriptor command) {
+    public int[] rasterizeHeightMapRGB(@NonNull final HeightMapDescriptor command) {
         final int width = command.getHeightMap().length;
         final int height = command.getHeightMap()[0].length;
         final int[] imageBuffer = new int[width * height];
@@ -86,15 +51,15 @@ public class HeightmapRasterizer {
 
                 if (heightValue >= command.getSeaLevel()) {
                     // map height to color
-                    final float dynamicHeightValue = (heightValue - command.getSeaLevel()) / heightRange;
-                    int grayValue = (int) (255 * dynamicHeightValue); // normalize to 0..255
+                    final float dynamicHeightUnit = (heightValue - command.getSeaLevel()) / heightRange;
+                    int grayValue = (int) (255 * dynamicHeightUnit); // normalize to 0..255
                     grayValue = Math.min(Math.max(grayValue, 0), 255); // ensure that the value is in the valid range
                     color = new Color(grayValue, grayValue, grayValue);
                 } else {
                     // map depth to water color
-                    final float dynamicDepthValue = (heightValue - command.getSeaLevel()) / depthRange;
-                    int blueValue = (int) (255 * (dynamicDepthValue)); //  // normalize to 0..255
-                    blueValue = Math.min(Math.max(blueValue, 0), 255);
+                    final float dynamicDepthUnit = (heightValue - command.getSeaLevel()) / depthRange;
+                    int blueValue = (int) (255 * (dynamicDepthUnit)); //  // normalize to 0..255
+                    blueValue = Math.min(Math.max(blueValue, 0), 255); // ensure that the value is in the valid range
                     color = new Color((int) (blueValue * 0.77), (int) (192 * 0.94), blueValue);
                 }
 
