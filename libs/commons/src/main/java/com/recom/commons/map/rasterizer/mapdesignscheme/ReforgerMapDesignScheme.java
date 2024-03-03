0d7084b42e0c648262cef6428705d1caa3e32620
Sven Carrillo Castillo <carrillo.castillo.sven@googlemail.com>
2024-03-03 14:02:50 +0100

    update ReforgerMapDesignScheme

--- libs/commons/src/main/java/com/recom/commons/map/rasterizer/mapdesignscheme/ReforgerMapDesignScheme.java
+++ libs/commons/src/main/java/com/recom/commons/map/rasterizer/mapdesignscheme/ReforgerMapDesignScheme.java
@@ -12,18 +12,18 @@ public class ReforgerMapDesignScheme extends MapDesignScheme {
 
     // Contour line config
     private final int baseColorContourBackground = 0x00000000;
-    private final int baseColorContourLineTerrain = 0x55E2A750;
+    private final int baseColorContourLineTerrain = 0x44BEAA8C;
     private final int baseColorContourLineCoast = 0xAA597482;
-    private final int baseColorContourLineWater = 0x33506975; // #7396A8, #506975
-    private final float brightnessModifierPrimaryLinesAboveSeaLevel = 0.25f;
-    private final float brightnessModifierSecondaryLinesAboveSeaLevel = 0.1f;
-    private final float brightnessModifierPrimaryLinesBelowSeaLevel = 1f;
-    private final float brightnessModifierSecondaryLinesBelowSeaLevel = 0.25f;
+    private final int baseColorContourLineWater = 0x22597482;
+    private final float brightnessModifierPrimaryLinesAboveSeaLevel = 0.6f;
+    private final float brightnessModifierSecondaryLinesAboveSeaLevel = 1f;
+    private final float brightnessModifierPrimaryLinesBelowSeaLevel = 0.25f;
+    private final float brightnessModifierSecondaryLinesBelowSeaLevel = 1f;
     private final int contourLineStepSize = 20;
 
     // Sun config (shadowing)
     private final int sunAzimutDeg = 315; // usually 315
-    private final int sunElevationDeg = 30; // usually 30-45
+    private final int sunElevationDeg = 45; // usually 30-45
     private final int shadowMapAlpha = 0x66FFFFFF; // 0x66E2E2E2;
 
 }
