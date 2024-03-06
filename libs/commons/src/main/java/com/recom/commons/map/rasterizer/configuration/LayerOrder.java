9f99278c1d247f08287922b75d1da5ca9ef8ba3d
Sven Carrillo Castillo <carrillo.castillo.sven@googlemail.com>
2024-03-06 22:31:44 +0100

    update LayerOrder of renderer

--- libs/commons/src/main/java/com/recom/commons/map/rasterizer/configuration/LayerOrder.java
+++ libs/commons/src/main/java/com/recom/commons/map/rasterizer/configuration/LayerOrder.java
@@ -9,8 +9,8 @@ public class LayerOrder {
     public static int SLOPE_MAP = 110;
     public static int BASE_MAP = 200;
     public static int HEIGHT_MAP = 300;
-    public static int SHADOWED_MAP = 400;
-    public static int FOREST_MAP = 500;
+    public static int FOREST_MAP = 400;
+    public static int SHADOWED_MAP = 500;
     public static int CONTOUR_MAP = 1000;
 
 }
